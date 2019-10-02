76a88864afda5d8ed72a27820cefc8ae976e38ce
Ivan5 <converse_ivan@hotmail.com>
2019-10-02 16:23:46 -0500

    Make findAllProjects function

--- PPMToolFullStack/src/main/java/com/example/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -41,4 +41,10 @@ public class ProjectController {
 
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){
+        return projectService.findAllProject();
+    }
+
 }
